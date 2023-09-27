9bd618b44ffea4c78e6daed25d2e5398c5963fd4
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-09-27 11:21:39 +0100

    feat: make faqs section more functional

--- src/script.js
+++ src/script.js
@@ -1,12 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerMenu = document.querySelector('#hamburgerMenu');
     const closeMenuBtn = document.querySelector('#closeMenu');
+    const faqQuestions = document.querySelectorAll('.faq__questions');
     
-    document.querySelector('#toggleMenu').onclick = () => {
+    document.querySelector('#toggleMenu').addEventListener('click', () => {
         hamburgerMenu.style.top = 0;
-    }
+    })
     
-    closeMenuBtn.onclick = () => {
+    closeMenuBtn.addEventListener('click', () => {
         hamburgerMenu.style.top = '-100%';
-    }
+    })
+    
+    faqQuestions.forEach(question => {
+        question.addEventListener('click', function() {
+            const answer = this.nextElementSibling;    
+            
+            this.classList.toggle('open');
+
+            
+            if (this.classList.contains('open')) {
+                this.querySelector('span').innerHTML = '-'
+                this.nextElementSibling.classList.toggle('hidden')
+                setTimeout(() => {
+                    this.nextElementSibling.classList.toggle('opacity-0')
+                }, 200)
+            } else {
+                this.nextElementSibling.classList.toggle('opacity-0')
+                setTimeout(() => {
+                    this.nextElementSibling.classList.toggle('hidden')
+                }, 200)
+                this.querySelector('span').innerHTML = '+'
+            }
+
+        })
+    })
 })
